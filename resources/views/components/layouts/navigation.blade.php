6f4289918d26d6b8401bea6e77981211d0b22d4f
Vicos <victor_joel_elbarto@hotmail.com>
2022-08-27 19:17:39 -0400

    Se agrego las propiedades del .env

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -3,7 +3,7 @@
     <ul>
         <li><a href="{{route('home')}}">Home</a></li>
         <li><a href="{{route('blog')}}">Blog</a></li>
-        <li><a href="{{route('about')}}">About</a></li>
+        <li><a href="{{route('asdas')}}">About</a></li>
         <li><a href="{{route('contac')}}">contac</a></li>
     </ul>
 
